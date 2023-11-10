c578350997debc7d38e1f02fe83450270ccc2256
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-11-10 12:12:51 +0100

    Update CicdTemplateApplicationTests.java

--- src/test/java/com/pejomi/cicdtemplate/CicdTemplateApplicationTests.java
+++ src/test/java/com/pejomi/cicdtemplate/CicdTemplateApplicationTests.java
@@ -3,11 +3,14 @@ package com.pejomi.cicdtemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CicdTemplateApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(2, 1);
     }
 
 }
