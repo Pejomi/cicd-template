b57d03d2246bb7849a0483c6b6eb42c980510ce7
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-11-10 11:14:48 +0100

    init

---
+++ src/main/java/com/pejomi/cicdtemplate/CicdTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.pejomi.cicdtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pejomi/cicdtemplate/CicdTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pejomi.cicdtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
