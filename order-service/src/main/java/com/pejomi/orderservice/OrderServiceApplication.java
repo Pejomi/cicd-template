9fc37a5ccc28d2119b62f38502314a8c7601f969
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-11-10 14:23:36 +0100

    added microservices

--- order-service/src/main/java/com/pejomi/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/pejomi/orderservice/OrderServiceApplication.java
@@ -1,13 +1,12 @@
-package com.pejomi.cicdtemplate;
+package com.pejomi.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdTemplateApplication {
+public class OrderServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CicdTemplateApplication.class, args);
+        SpringApplication.run(OrderServiceApplication.class, args);
     }
-
 }
---
+++ order-service/src/main/java/com/pejomi/orderservice/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.pejomi.orderservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Order Service";
+    }
+
+
+}
